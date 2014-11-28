dea295e7e9ac2208c4874bd2e9f1b86d9b24cce8
Antoine Terrienne <antoine.terrienne@gmail.com>
2014-11-28 12:39:11 -0500

    Fix video jitter. Fix define names. Simplify few things.

--- vgarduino.ino
+++ vgarduino.ino
@@ -24,6 +24,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * 
  * -- History --
+ * (2014/11/27) v1.1 - make output more stable, fix horizontal/vertical inversion in variable naming.
  * (2014/02/09) v1.0 - outputs a simple pattern of 8 color bars
  * 
  * -- VGA connector pinout --
@@ -126,175 +127,190 @@
 
 #include <avr/interrupt.h>
 
+#define CONCAT(a, b)  a##b
+#define PORT(p)  CONCAT(PORT,p)
+#define DDR(p)  CONCAT(DDR,p)
+
 #define RGB_PORT   D
 #define RED_PIN    7
 #define GREEN_PIN  6
 #define BLUE_PIN   5
 
 #define SYNCH_PORT B
-#define VSYNCH_PIN 1
-#define HSYNCH_PIN 0
+#define HSYNCH_PIN 1
+#define VSYNCH_PIN 0
 
-#define CONCAT(a, b)  a##b
-#define PORT(p)  CONCAT(PORT,p)
-#define DDR(p)  CONCAT(DDR,p)
+#define BLACK_MASK   0
+#define RED_MASK     _BV(RED_PIN)
+#define GREEN_MASK   _BV(GREEN_PIN)
+#define BLUE_MASK    _BV(BLUE_PIN)
+#define CYAN_MASK    (_BV(GREEN_PIN)|_BV(BLUE_PIN))
+#define YELLOW_MASK  (_BV(RED_PIN)|_BV(GREEN_PIN))
+#define MAGENTA_MASK (_BV(RED_PIN)|_BV(BLUE_PIN))
+#define WHITE_MASK   (_BV(RED_PIN)|_BV(GREEN_PIN)|_BV(BLUE_PIN))
 
-#define BLACK   0
-#define RED     _BV(RED_PIN)
-#define GREEN   _BV(GREEN_PIN)
-#define BLUE    _BV(BLUE_PIN)
-#define CYAN    (_BV(GREEN_PIN)|_BV(BLUE_PIN))
-#define YELLOW  (_BV(RED_PIN)|_BV(GREEN_PIN))
-#define MAGENTA (_BV(RED_PIN)|_BV(BLUE_PIN))
-#define WHITE   (_BV(RED_PIN)|_BV(GREEN_PIN)|_BV(BLUE_PIN))
-
-#define SYNCH_PULSE  0
-#define BACK_PORCH   1
-#define ACTIVE_VIDEO 2
-#define FRONT_PORCH  3
-
-#define H_FRONT_PORCH_LINES  11
-#define H_SYNCH_PULSE_LINES  2
-#define H_BACK_PORCH_LINES   31
-#define H_ACTIVE_VIDEO_LINES 480
+#define V_FRONT_PORCH_LINES  11
+#define V_SYNCH_PULSE_LINES  2
+#define V_BACK_PORCH_LINES   31
+#define V_ACTIVE_VIDEO_LINES 480
 
 int nb_line_modes[] = {
-  H_SYNCH_PULSE_LINES,
-  H_BACK_PORCH_LINES,
-  H_ACTIVE_VIDEO_LINES,
-  H_FRONT_PORCH_LINES,
+#define SYNCH_PULSE  0
+  V_SYNCH_PULSE_LINES,
+#define BACK_PORCH   1
+  V_BACK_PORCH_LINES,
+#define ACTIVE_VIDEO 2
+  V_ACTIVE_VIDEO_LINES,
+  V_FRONT_PORCH_LINES,
+#define FRONT_PORCH  3
 };
 
-#define V_SYNCH_PULSE_TICKS  61
-#define V_BACK_PORCH_TICKS   30
-#define V_ACTIVE_VIDEO_TICKS 406
-#define V_FRONT_PORCH_TICKS  10
-#define V_FULL_LINE_NB_TICKS (V_SYNCH_PULSE_TICKS + \
-                              V_BACK_PORCH_TICKS +  \
-                              V_ACTIVE_VIDEO_TICKS + \
-                              V_FRONT_PORCH_TICKS)
+#define H_SYNCH_PULSE_TICKS  61
+#define H_BACK_PORCH_TICKS   30
+#define H_ACTIVE_VIDEO_TICKS 406
+#define H_FRONT_PORCH_TICKS  10
+#define H_FULL_LINE_NB_TICKS (H_SYNCH_PULSE_TICKS + \
+                              H_BACK_PORCH_TICKS +  \
+                              H_ACTIVE_VIDEO_TICKS + \
+                              H_FRONT_PORCH_TICKS)
 
 #define WAIT_VIDEO_INACTIVE()  do {} while (phase == ACTIVE_VIDEO)
 #define WAIT_VIDEO_ACTIVE()  do {} while (phase != ACTIVE_VIDEO)
 
 byte phase;
-int line_number;
+int lines_left;
 
 inline void incrementLineNumber() {
-  line_number++;
-  if (line_number == nb_line_modes[phase]) {
-    line_number = 0;
+  lines_left--;
+  if (lines_left == 0) {
     phase++;
-    phase &= 0x03; // Take adventage that we have 4 phases.
+    phase &= 0x03; // Take adventage that we have 4 phases only.
+    lines_left =  nb_line_modes[phase];
   }
 }
 
+/*inline void waitTimer1(byte tick) {
+  do { } while(TCNT1L < tick);
+}*/
+
+/* Wait for TCNT1 to be equal to tick
+* this is a way to synchronise with the
+* timer, because the interrupt can be triggered with a delay
+* we have to compensate to make sure we execute the color changing
+* commands at the same offset from the synch.
+*
+* we first get the difference between the current TCNT1 timer value
+* and the expected one, this will give us an exact the number of ticks
+* to wait.
+* Then we jump threw some hoops to make sure the consume onyl the
+* required numbers of ticks to synch the execution flow with TCNT1 counter.
+*
+* This version only checks TCNT1L, as we won't need to synch with a value
+* greater than 255.
+*/
 inline void waitTimer1(byte tick) {
-  do { } while(TCNT1L < tick );
+  asm volatile(
+  // we count clock cycles for each instruction to synch-up with TCNT1L
+  "sub %[tick], %[tmp]\n\t"// 1   +1=1
+  "sub %[tick], 13\n\t"    // 1   +1=2 green path consumes 12 ticks whatever happens
+  "rjmp 2f\n\t"            // 2   +2=4
+  "1: subi %[tick], 4\n\t" // 1        each loop to reduce the ticks consume 4 clocks
+  "2: cpi %[tick], 4\n\t"  // 1   +1=5
+  "brcc 1b\n\t"            // 1/2 +1=6
+  //     at this point,  tick can be =    0     1     2     3
+  "cpi %[tick], 0\n\t"     // 1        +1=1  +1=1  +1=1  +1=1
+  "breq 3f\n\t"            // 1/2      +2=3  +1=2  +1=2  +1=2
+  "cpi %[tick], 1\n\t"     // 1              +1=3  +1=3  +1=3
+  "breq 1f\n\t"            // 1/2            +2=5  +1=4  +1=4
+  "cpi %[tick], 2\n\t"     // 1                    +1=5  +1=5
+  "breq 2f\n\t"            // 1/2                  +2=7  +1=6
+  "0: nop\n\t"             // 1        +1=4              +1=7
+  "1: nop\n\t"             // 1        +1=5  +1=6        +1=8
+  "2: nop\n\t"             // 1        +1=6  +1=7  +1=8  +1=9
+  "3:"                     //          -0=6  -1=6  -2=6  -3=6
+  : 
+  : [tick] "r" (tick),
+    [tmp] "r" (TCNT1L)
+  );
 }
 
-// vertical sync pulse start
+// horizontal line start
 ISR(TIMER1_CAPT_vect) {
-  // wait for the end of the back porch
-  waitTimer1(V_SYNCH_PULSE_TICKS+V_BACK_PORCH_TICKS);
-  
-  switch (phase) {
-    case FRONT_PORCH:
-      if (line_number == (H_FRONT_PORCH_LINES - 1)) {
-        // end of front porch, start of Vsynch pulse
-        bitClear(PORT(SYNCH_PORT), HSYNCH_PIN);
+  if (phase == ACTIVE_VIDEO) {
+        // wait for the end of the back porch
+        waitTimer1(H_SYNCH_PULSE_TICKS+H_BACK_PORCH_TICKS);
+        PORT(RGB_PORT) = RED_MASK;
+        delayMicroseconds(3);
+        PORT(RGB_PORT) = GREEN_MASK;
+        delayMicroseconds(3);
+        PORT(RGB_PORT) = BLUE_MASK;
+        delayMicroseconds(3);
+        PORT(RGB_PORT) = CYAN_MASK;
+        delayMicroseconds(3);
+        PORT(RGB_PORT) = YELLOW_MASK;
+        delayMicroseconds(3);
+        PORT(RGB_PORT) = MAGENTA_MASK;
+        delayMicroseconds(3);
+        PORT(RGB_PORT) = BLACK_MASK;
+        delayMicroseconds(3);
+        PORT(RGB_PORT) = WHITE_MASK;
+        delayMicroseconds(3);
+        PORT(RGB_PORT) = 0;
+  } else if (phase == FRONT_PORCH || phase == SYNCH_PULSE) {
+      if (lines_left == 1) {
+        // wait for the end of the back porch
+        waitTimer1(H_SYNCH_PULSE_TICKS+H_BACK_PORCH_TICKS);
+        // if end of front porch, set Vsynch to 0 to start synch pulse
+        // if enf of synch pulse, set Vsynch to 1 to start back porch
+        PORT(SYNCH_PORT) ^= _BV(VSYNCH_PIN);
       }
-      break;
-    case SYNCH_PULSE:
-      if (line_number == (H_SYNCH_PULSE_LINES - 1)) {
-        // end of Vsynch pulse, start of back porch
-        bitSet(PORT(SYNCH_PORT), HSYNCH_PIN);
-      }
-      break;
-    case ACTIVE_VIDEO:
-      {
-        PORT(RGB_PORT) = RED;
-        waitTimer1(
-          V_SYNCH_PULSE_TICKS+
-          V_BACK_PORCH_TICKS+
-          (V_ACTIVE_VIDEO_TICKS/8)
-        );
-        PORT(RGB_PORT) = GREEN;
-        waitTimer1(
-          V_SYNCH_PULSE_TICKS+
-          V_BACK_PORCH_TICKS+
-          ((V_ACTIVE_VIDEO_TICKS * 2)/8)
-        );
-        PORT(RGB_PORT) = BLUE;
-        waitTimer1(
-          V_SYNCH_PULSE_TICKS+
-          V_BACK_PORCH_TICKS+
-          ((V_ACTIVE_VIDEO_TICKS * 3)/8)
-        );
-        PORT(RGB_PORT) = CYAN;
-        waitTimer1(
-          V_SYNCH_PULSE_TICKS+
-          V_BACK_PORCH_TICKS+
-          ((V_ACTIVE_VIDEO_TICKS * 4)/8)
-        );
-        PORT(RGB_PORT) = YELLOW;
-        waitTimer1(
-          V_SYNCH_PULSE_TICKS+
-          V_BACK_PORCH_TICKS+
-          ((V_ACTIVE_VIDEO_TICKS * 5)/8)
-        );
-        PORT(RGB_PORT) = MAGENTA;
-        waitTimer1(
-          V_SYNCH_PULSE_TICKS+
-          V_BACK_PORCH_TICKS+
-          ((V_ACTIVE_VIDEO_TICKS * 6)/8)
-        );
-        PORT(RGB_PORT) = BLACK;
-        waitTimer1(
-          V_SYNCH_PULSE_TICKS+
-          V_BACK_PORCH_TICKS+
-          ((V_ACTIVE_VIDEO_TICKS * 7)/8)
-        );
-        PORT(RGB_PORT) = WHITE;
-        waitTimer1(
-          V_SYNCH_PULSE_TICKS+
-          V_BACK_PORCH_TICKS+
-          V_ACTIVE_VIDEO_TICKS
-        );
-        PORT(RGB_PORT) = (_BV(VSYNCH_PIN)|_BV(HSYNCH_PIN));
-      }
-      break;
   }
   incrementLineNumber();
 }
 
-void init_video() {
+
+inline void init_io() {
   // Set pins for color and synch as output
   DDR(RGB_PORT) = (_BV(RED_PIN)|_BV(GREEN_PIN)|_BV(BLUE_PIN));
   DDR(SYNCH_PORT) = (_BV(VSYNCH_PIN)|_BV(HSYNCH_PIN));
   
   PORT(RGB_PORT) = 0;
   PORT(SYNCH_PORT) = (_BV(VSYNCH_PIN)|_BV(HSYNCH_PIN));
-  
+}
+
+/*
+* we are using the TIMER1 to generate the horizontal lines
+*/
+inline void init_video_line_timer() {
+  TCCR0A = 0; // disable timer0, it causes issues to trigger interrupt
+  TCCR0B = 0;
   TCCR1A = 0; // disable all first.
   TCCR1B = 0;
   TCCR1C = 0;
-  cli();
   // init line/phase status
-  line_number = 0;
   phase = FRONT_PORCH;
+  lines_left =  nb_line_modes[phase];
   // init timing
+  // reset counter to 0
   TCNT1 = 0;
-  OCR1A = V_SYNCH_PULSE_TICKS;
-  ICR1 = V_FULL_LINE_NB_TICKS;
+  // set first match
+  OCR1A = H_SYNCH_PULSE_TICKS;
+  // set second match to the end of the video line
+  // counter 
+  ICR1 = H_FULL_LINE_NB_TICKS;
+  // Enable the interrupt
   TIMSK1 = _BV(ICIE1);
+  // set mode to FastPWN mode
+  // 
   TCCR1A = _BV(COM1A1)|_BV(COM1A0)|_BV(WGM11);
   TCCR1B = _BV(WGM13)|_BV(WGM12)|_BV(CS10);
-  sei();
 }
 
 void setup () {
-  init_video();
+  init_io();
+  cli();
+  init_video_line_timer();
+  sei();
 }
 
 void loop () {
